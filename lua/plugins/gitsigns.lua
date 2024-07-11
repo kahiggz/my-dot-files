4ded47f14f8dde85658eb8fa355a67bff0a7a9f3
Bryan Higgz <bryanhiggz1@gmail.com>
2024-07-11 14:44:43 +0300

    additions

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,5 +5,6 @@ return {
 
     local keymap = vim.keymap
     keymap.set("n", "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "toggle line blame" })
+    keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "preview changes" })
   end,
 }

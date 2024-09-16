bd8e0b894f518455e18cb940b41cf37ca696281d
Bryan Higgz <bryanhiggz1@gmail.com>
2024-09-16 11:43:53 +0300

    Introduce the oilnvim plugin and test

--- lua/plugins/lsp/mason_nvim.lua
+++ lua/plugins/lsp/mason_nvim.lua
@@ -1,20 +1,3 @@
--- return {
---   "williamboman/mason.nvim",
---   config = function()
---     local mason = require("mason")
---     -- local mason_tool_installer = require("mason-tool-installer")
---     mason.setup({
---       ui = {
---         icons = {
---           package_installed = "✓",
---           package_pending = "➜",
---           package_uninstalled = "✗",
---         },
---       },
---     })
---   end,
--- }
---
 return {
   "williamboman/mason.nvim",
   dependencies = {
